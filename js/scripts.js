314c5d65bee395deb79440ff31cd2ff1c3a04b68
frontenddevproject <frontenddevproject@gmail.com>
2023-03-31 13:06:07 +0300

    added completed homework

--- js/scripts.js
+++ js/scripts.js
@@ -2,34 +2,32 @@
 function MyClass (a, privateArguments) {
    this.a = a;
    
-    const keysArray = privateArguments.reduce((acc, el, i, arr) => {
-      let data = Object.keys(Object.values(arr)[i]).join();
-      if(!acc.includes(data)){
-         acc.push(data);   
-      }
-      return acc;
-   }, [])
-   for(let i = 0; i < privateArguments.length; i++){
-   // if ()
-   }
-	console.log(keysArray)
-   console.log(Object.keys(Object.values(privateArguments)))
+	function checkArguments (arrayArguments) {
+		let acc = [];
+		let newArr = [];
+		arrayArguments.forEach((el, i) => {
+			let data = Object.keys(el).join();
+			if (!acc.includes(data)) {
+				acc.push(data); 
+				newArr.push(el)
+			} 
+			return  newArr
+		})
+		privateArguments = newArr
+		return  privateArguments
+	}
+	
+	checkArguments(privateArguments)
 
-
-//  Еще в разработке, до конца дня сделаю
-
-
-   // console.log(Object.keys(Object.values(privateArguments)[2]));
-   // privateArguments.forEach((obj) => {
-   //   const key = Object.keys(obj)[0];
-   //   const value = Object.values(obj)[0];
-   //   // this[key] = value
-   //   Object.defineProperty(this, key, {value: value, enumerable: false, writable: false});
-   // }) 
-   // return this;
+   privateArguments.forEach((obj) => {
+     const key = Object.keys(obj)[0];
+     const value = Object.values(obj)[0];
+     Object.defineProperty(this, key, {value: value, enumerable: false, writable: false});
+   }) 
+   return this;
 
 }
 
-const instanceClass = new MyClass(10, [{private: "private"}, {private: "private2"}, {private2: "private2"},{private2: "private2"}]);
+const instanceClass = new MyClass(10, [{private: "private"}, {private: "private2"}, {private2: "private2"}]);
 
 console.log(instanceClass, "instance");
